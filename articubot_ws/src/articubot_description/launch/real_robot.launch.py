36952b2768cd523213a48b755ffc2bdd7b2f3c99
Thompson <bathompson@ksu.edu>
2024-11-20 17:05:25 -0600

    Added camera node to bringup

--- articubot_ws/src/articubot_description/launch/real_robot.launch.py
+++ articubot_ws/src/articubot_description/launch/real_robot.launch.py
@@ -79,6 +79,12 @@ def generate_launch_description():
         parameters=[robot_description, {"use_sim_time": False}],
     )
 
+    camera_node = Node(
+        package="v4l2_camera",
+        executable="v4l2_camera_node",
+        parameters=[{"image_size": "[640,480]"}]
+    )
+
     # ROS2-Gazebo Bridge
     # bridge = Node(
     #     package='ros_gz_bridge',
